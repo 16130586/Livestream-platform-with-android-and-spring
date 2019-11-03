03e41e4b41c2bfc521e4d6b79141ad2a96af6455
Tuyen <nhoktuyenpro@gmail.com>
2019-11-03 12:59:48 +0700

    change db init docker

--- docker-storage/mysql/init/init.sql
+++ docker-storage/mysql/init/init.sql
@@ -7,91 +7,133 @@ flush privileges;
 CREATE SCHEMA IF NOT EXISTS `livestream`;
 USE `livestream`;
 
-CREATE TABLE IF NOT EXISTS `User` (
-  `user_id` integer,
-  `username` varchar(255),
-  `password` varchar(255),
-  `nick_name` varchar(255),
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
   `avatar` blob,
-  `gmail` varchar(255),
-  `forgot_token` varchar(255),
-  `subcribe_total` integer
-);
-
-CREATE TABLE IF NOT EXISTS `subscriber` (
-  `subcriber_id` integer,
-  `publisher_id` integer
-);
-
-CREATE TABLE IF NOT EXISTS `types` (
-  `type_id` integer,
-  `type_name` varchar(255)
+  `gmail` varchar(255) DEFAULT NULL,
+  `forgot_token` varchar(255) DEFAULT NULL,
+  `subscribe_total` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `stream` (
-  `stream_id` integer,
-  `wowza_id` varchar(255),
-  `owner_id` integer,
-  `total_view` integer,
-  `start_time` datetime,
-  `end_time` datetime,
-  `stream_status` integer,
+  `stream_id` int(11) NOT NULL AUTO_INCREMENT,
+  `wowza_id` varchar(255) DEFAULT NULL,
+  `owner_id` int(11) DEFAULT NULL,
+  `total_view` int(11) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `stream_status` int(11) DEFAULT NULL,
   `forwards` text,
   `forwards_url` text,
-  `stored_url` text
+  `stored_url` text,
+  PRIMARY KEY (`stream_id`),
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `stream_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `stream_type` (
-  `stream_id` integer,
-  `type_id` integer
-);
-
-CREATE TABLE IF NOT EXISTS `user_favorite` (
-  `user_id` integer,
-  `type_id` integer
-);
-
-CREATE TABLE IF NOT EXISTS `notification` (
-  `id` integer,
-  `stream_id` integer,
-  `message` varchar(255),
-  `status` integer,
-  `created_time` datetime
-);
-
-CREATE TABLE IF NOT EXISTS `notification_user` (
-  `notify_id` integer,
-  `user_id` integer
+CREATE TABLE IF NOT EXISTS `types` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`type_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `comment` (
-  `comment_id` integer,
-  `owner_id` integer,
-  `stream_id` integer,
-  `message` varchar(255),
-  `created_time` datetime,
-  `status` integer,
-  `source` integer
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) DEFAULT NULL,
+  `stream_id` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `source` int(11) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `owner_id` (`owner_id`),
+  KEY `stream_id` (`stream_id`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`stream_id`) REFERENCES `stream` (`stream_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `favorite_saved` (
-  `user_id` integer,
-  `stream_id` integer,
-  `saved_time` datetime,
-  `status` integer
+CREATE TABLE IF NOT EXISTS `favourite_saved` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `stream_id` int(11) DEFAULT NULL,
+  `saved_time` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `stream_id` (`stream_id`),
+  CONSTRAINT `favourite_saved_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `favourite_saved_ibfk_2` FOREIGN KEY (`stream_id`) REFERENCES `stream` (`stream_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stream_id` int(11) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stream_id_fk3_idx` (`stream_id`),
+  CONSTRAINT `stream_id_fk3` FOREIGN KEY (`stream_id`) REFERENCES `stream` (`stream_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `notification_user` (
+  `notify_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  KEY `notify_id` (`notify_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `notification_user_ibfk_1` FOREIGN KEY (`notify_id`) REFERENCES `notification` (`id`),
+  CONSTRAINT `notification_user_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `stream_type` (
+  `stream_id` int(11) NOT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  KEY `stream_id` (`stream_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `stream_type_ibfk_1` FOREIGN KEY (`stream_id`) REFERENCES `stream` (`stream_id`),
+  CONSTRAINT `stream_type_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `subscriber` (
+  `subscriber_id` int(11) DEFAULT NULL,
+  `publisher_id` int(11) DEFAULT NULL,
+  KEY `subscriber_id` (`subscriber_id`),
+  KEY `publisher_id` (`publisher_id`),
+  CONSTRAINT `subscriber_ibfk_1` FOREIGN KEY (`subscriber_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `subscriber_ibfk_2` FOREIGN KEY (`publisher_id`) REFERENCES `user` (`user_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `subscription` (
-  `id` integer,
-  `name` varchar(255),
-  `type` integer,
-  `unit_price` double
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `unit_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `pay_subscription` (
-  `subscription_id` integer,
-  `user_id` integer,
-  `start_time` datetime,
-  `end_time` datetime,
-  `amount` double
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subscription_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `subscription_id` (`subscription_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `pay_subscription_ibfk_1` FOREIGN KEY (`subscription_id`) REFERENCES `subscription` (`id`),
+  CONSTRAINT `pay_subscription_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `user_favourite` (
+  `user_id` int(11) DEFAULT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `type_id` (`type_id`),
+  CONSTRAINT `user_favourite_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `user_favourite_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`)
 );
