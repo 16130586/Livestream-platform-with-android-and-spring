e5c0579fcec2547817a674df533bdfc3b361cd00
mrlittle113 <43637889+mrlittle113@users.noreply.github.com>
2019-12-24 15:49:25 +0700

    host api

--- android-client/app/src/main/java/com/t4/androidclient/contraints/Host.java
+++ android-client/app/src/main/java/com/t4/androidclient/contraints/Host.java
@@ -1,8 +1,6 @@
 package com.t4.androidclient.contraints;
 
 public class Host {
-//    public static final String API_HOST_IP = "http://192.168.110.8:8080";
-//    // "http://192.168.100.13:8080"
-
-    public static final String API_HOST_IP = "http://6ebce9a9.ngrok.io";
+    public static final String API_HOST_IP = "http://192.168.1.4:8080";
+    public static final String SOCKET_HOST = "http://192.168.1.4:3000";
 }
