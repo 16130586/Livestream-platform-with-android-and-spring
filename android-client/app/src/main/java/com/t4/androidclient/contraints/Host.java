396f0aec62a1eba3646b46e37b2de708eabd4076
Tuyen <nhoktuyenpro@gmail.com>
2019-12-25 10:47:20 +0700

    fix some mistake

--- android-client/app/src/main/java/com/t4/androidclient/contraints/Host.java
+++
@@ -1,8 +0,0 @@
-package com.t4.androidclient.contraints;
-
-public class Host {
-   public static final String API_HOST_IP = "http://192.168.1.5:8080";
-
-    public static final String SOCKET_HOST = "http://192.168.1.5:3000";
-
-}
