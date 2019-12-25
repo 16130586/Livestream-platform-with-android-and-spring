6e46310b18c1ee9a7524d3c8fb3f7fb80b50df44
Tuyen <nhoktuyenpro@gmail.com>
2019-12-25 10:48:18 +0700

    add Host again

---
+++ android-client/app/src/main/java/com/t4/androidclient/contraints/Host.java
@@ -0,0 +1,8 @@
+package com.t4.androidclient.contraints;
+
+public class Host {
+   public static final String API_HOST_IP = "http://192.168.1.194:8080";
+
+    public static final String SOCKET_HOST = "http://192.168.1.5:3000";
+
+}
